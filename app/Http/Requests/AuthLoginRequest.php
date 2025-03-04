b0e43759f3276fb4d930522fb6147a9a4a9393b5
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-04 22:33:02 +0500

    create Login validate request

--- app/Http/Requests/AuthLoginRequest.php
+++ app/Http/Requests/AuthLoginRequest.php
@@ -22,7 +22,8 @@ class AuthLoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            
+            'email'=>'required|email',
+            'password'=>'required|min:6'
         ];
     }
 }
