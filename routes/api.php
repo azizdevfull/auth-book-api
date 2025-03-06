b095befba48f59dbe598ff279d174beea9c954f0
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-06 21:28:20 +0500

    created routes with middleware

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('posts', [BookController::class, 'store']);
+    Route::get('posts', [BookController::class, 'index']);
+    Route::get('posts/search', [BookController::class, 'search']);
+    Route::get('posts/{post}', [BookController::class, 'show']);   
+    Route::put('posts/{post}', [BookController::class, 'update']);
+    Route::delete('posts/{post}', [BookController::class, 'destroy']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
