5ff69e345c2e9661a79e459697be84a723afb701
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-06 21:27:41 +0500

    changed +image

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,5 +8,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Book extends Model
 {
     use HasFactory, Notifiable;
-    
+    protected $fillable = [
+        'name',
+        'description',
+        'author_id'
+    ];
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+    public function author()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
