0e4f03372a48110daaa3dfe9ea149191a1631e55
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-06 21:26:08 +0500

    changed AuthController +register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Image;
 use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\AuthLoginRequest;
 use App\Http\Requests\AuthRegisterRequest;
@@ -12,32 +14,47 @@ class AuthController extends Controller
 {
     public function register(AuthRegisterRequest $request)
     {
-        $user =User::create([
+        $user=User::create([
             'name'=>$request->name,
             'email'=>$request->email,
             'password'=>bcrypt( $request->password),
+        ]);
+    
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imagePath = $this->uploadPhoto($image, 'avatars'); 
+    
+            Image::create([
+                'path' => $imagePath,
+                'imageable_id' => $user->id,
+                'imageable_type' => User::class, 
             ]);
-            $token =$user->createToken('auth_token')->plainTextToken;
-            return response()->json([
-                'token'=>$token,
-                
-            ]);
+        $token =$user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'success'=>true,
+            'token'=>$token,
+            'user'=> new UserResource($user->load('avatar'))
+        ]);
+    }
     }
     public function login(AuthLoginRequest $request)
     {
         $user=User::where('email',$request->email)->first();
-
-        if(!$user ||!Hash::chek($request->password,$user->password)){
-            return response()->json([
-                'message' => 'Notogri Email yoki parol'
-            ], 401);
+    if(!$user || !Hash::check($request->password,$user->password))
+    {
+        return response()->json(
+            [
+                'success'=>false
+            ],401
+            );
     }
-    $user->tokens->delete();
-    $token =$user->createToken('auth_token')->plainTextToken;
-
+    $user->tokens()->delete();
+    $token=$user->createToken('auth_book')->plainTextToken;
     return response()->json([
+        'success'=>true,
         'token'=>$token,
-        'user'=>$user
+        'user'=> new UserResource($user->load('avatar'))
+
     ]);
     }
     public function logout(Request $request){
