70704e6d75dc783e48689662dc63ca75750fc9e8
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-06 21:27:52 +0500

    changed +relation

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,17 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Image extends Model
 {
     use HasFactory, Notifiable;
-    protected $fillable = ['imageable_id', 'imageable_type', 'path'];
+    protected $fillable = [
+        'imageable_id',
+        'imageable_type',
+        'path'
+    ];
 
     public function user()
 
     {
         return $this->belongsTo(User::class);
+    }
+    public function imageable()
+    {
+        return $this->morohTo();
+    }
+    public function url()
+    {
+        return URL::to('storage/' . $this->path);
     }
 }
