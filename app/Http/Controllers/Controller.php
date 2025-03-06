f7aab3e3f05fd64541ee7adefba4556773ba5079
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-06 21:26:26 +0500

    changed

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,16 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    
+    public function uploadPhoto($file, $path="uploads"){
+        $photoName=md5(time() . $file->getFilename()) . '.' . $file->getClientOriginalExtension();
+        return $file->storeAs($path, $photoName,'public');
+    }
+    public function deletePhoto($path){
+        $fullpath = public_path('storage/' . $path);
+
+        if(file_exists($fullpath)){
+            unlink($fullpath);
+        }
+
+    }
 }
